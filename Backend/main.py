6750bba184d0601513692e280a139e2317dc7263
piyushchaudhari04 <piyush.kailash.chaudhari@gmail.com>
2020-03-24 19:56:57 +0530

    ml model inference api skeleton code

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+
+from .ml.model import Inference
 
 app = FastAPI()
+inference_obj = Inference()
 
 
 @app.get("/")
@@ -12,6 +16,13 @@ def show_main_page():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.get("/maps")
 def show_map():
+    pass
+
+
+@app.post("/maps/safepath")
+def get_safest_route(request: Request):
+    print (request.body())
     pass
