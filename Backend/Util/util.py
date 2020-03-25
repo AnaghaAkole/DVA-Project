c064e8f24dd9358a1b36f78c15ceca7ccf5dbfa4
Shivam Dhanadhya <shivam.dhanadhya@gmail.com>
2020-03-25 20:44:47 +0530

    Update util.py
    Add wrapper method for formatted input for prediction using ML model

--- Backend/Util/util.py
+++ Backend/Util/util.py
@@ -20,3 +20,25 @@ def get_weather_info(lattitude, longitude, time):
         filtered_data['day_night'] = 'Day'
     print(filtered_data)
     return filtered_data
+
+def predict_input_format_wrapper(attrs_dict):
+    """
+        This method parses attributes from dict to a list,
+        returned list can be use to predict Severity for an instance
+        Argument  : dict of input attributes with same naming as in the dataset
+        Return    : list of attributes to be passed to model.predict method
+    """
+    
+    feature_list = [attrs_dict['Start_Lng'], 
+                    attrs_dict['Start_Lat'],
+                    attrs_dict['Temperature(F)'],
+                    attrs_dict['Humidity(%)'],
+                    attrs_dict['Pressure(in)'],
+                    attrs_dict['Visibility(mi)'],
+                    float(attrs_dict['Crossing']),
+                    float(attrs_dict['Give_way']),
+                    float(attrs_dict['Railway']),
+                    float(attrs_dict['Station']),
+                    float(attrs_dict['Traffic_Calming']),
+                    float(attrs_dict['Traffic_Signal'])]
+    return feature_list
